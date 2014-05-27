167c7fb460db5176108975935b8d68bd6b62ba10
lftongheat <lftongheat@gmail.com>
2014-05-27 12:35:39 +0800

    Revert "card-onlinev1.0"
    This reverts commit 421441b212ce132adb664179f7438bb78b1d4d7a.

--- WZFSSDBPackagePayDlg/Des.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DES_
-#define _DES_
-
-void  DES(char *key,char *s_text,char *d_text);
-void  _DES(char *key,char *s_text,char *d_text);
-void  AdapteDES(char *key,int klen,char *s_text,int slen,char *d_text);
-void  _AdapteDES(char *key,int klen,char *s_text,int slen,char *d_text);
-
-#endif
